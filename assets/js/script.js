81894567f4f6cc20415ce3f9942a01bcf83412d3
akshith-07 <akshith2002jobirin@gmail.com>
2023-03-22 15:23:19 +0530

    INITIAL COMMIT

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+
+function printRelation(number, gender)
+{
+    let relation;
+    if(gender == 'f' && number > 0)
+    {
+        relation = "daughter"
+    }
+    else if(gender == 'f' && number < 0)
+    {
+        relation = "mother"
+    }
+    else if(gender == 'm' && number > 0)
+    {
+        relation = "son"
+    }
+    else
+    {
+        relation = "father"
+    }
+
+    switch(number)
+    {
+        case -3:
+        case 3:
+            console.log("great grand" + relation);
+            break;
+        case -2:
+        case 2:
+            console.log("grand" + relation);
+            break;
+        case -1:
+        case 1:
+            console.log(relation)
+            break;
+        case 0:
+            console.log("me");
+            break;
+    }
+
+}
+
+let number = 2
+let gender = 'm'
+
+printRelation(number, gender)
